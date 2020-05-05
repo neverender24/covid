7d4a01a94bfb168c683769bcc1338988569a8d4a
ikong <neverender24@gmail.com>
2020-05-05 12:30:53 +0800

    cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -18,6 +18,6 @@ class Cors
         return $next($request)
             ->header('Access-Control-Allow-Origin',"*")
             ->header('Access-Control-Allow-Methods',"GET, OPTIONS")
-            ->header('Access-Control-Allow-Header',"Accept, Authorization, Content-Type");
+            ->header('Access-Control-Allow-Headers',"Accept, Authorization, Content-Type");
     }
 }
